f651f7ed1dd417371c9673dc980c0093925a35d1
ggmini <minigsm14@gmail.com>
2026-01-31 17:10:56 +0100

    Added Rats

---
+++ Assets/Scripts/Rat.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+sealed class Rat : MonoBehaviour
+{
+    [SerializeField]
+    float dir = 1f;
+    [SerializeField]
+    float moveSpeed = 10f;
+
+    
+    void FixedUpdate() {
+        CheckInFront();
+        Move();
+    }
+
+    void CheckInFront() {
+        var origin = transform.position + new Vector3(dir * 0.75f, 0, 0);
+        var hitFront = Physics2D.Raycast(origin, Vector2.right * dir, 0.1f, LayerMask.GetMask("Environment"));
+        Debug.DrawRay(origin, Vector2.right * dir * 0.1f, Color.red);
+        origin.y -= 0.5f;
+        var hitDown = Physics2D.Raycast(origin, Vector2.down, 0.1f, LayerMask.GetMask("Environment"));
+        Debug.DrawRay(origin, Vector2.down * 0.1f, Color.red);
+        if (hitFront.collider != null || hitDown.collider == null) {
+            dir *= -1f;
+            var scale = transform.localScale;
+            scale.x *= -1f;
+            transform.localScale = scale;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Player")) {
+            Debug.Log("Rat collided with Player");
+            var player = other.gameObject.GetComponent<Player>();
+            player.TakeDamage(5);
+        }
+    }
+
+    void Move() {
+        transform.position += new Vector3(dir * moveSpeed * Time.deltaTime, 0, 0);
+    }
+
+}
